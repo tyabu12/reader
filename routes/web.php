f61296b125e1ce01425b4963e2c1103740758a8b
Tomohito YABU <tomotomo.y.1212@gmail.com>
2018-02-03 23:30:54 +0900

    feat: Add auth.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::view('/', 'welcome')->name('welcome');
+Route::get('/home', 'HomeController@index')->name('home');
