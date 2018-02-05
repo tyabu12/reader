4986d9ce6a320990b42e8c747a3666ff30e92d0a
tyabu12 <tomotomo.y.1212@gmail.com>
2018-02-05 20:48:07 +0900

    refactor: change button style on navbar.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,9 +35,6 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        <li><button class="btn btn-success" data-toggle="modal" data-target="#subscribeModal">
-                            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-                        </button></li>
                         <li><a href="{{ route('feeds.index') }}">{{ __('Feeds') }}</a></li>
                         <li><a href="{{ route('entries.index') }}">{{ __('Entries') }}</a></li>
                     </ul>
@@ -49,6 +46,12 @@
                             <li><a href="{{ route('login') }}">{{ __('Login') }}</a></li>
                             <li><a href="{{ route('register') }}">{{ __('Register') }}</a></li>
                         @else
+                            <li>
+                                <button class="btn btn-success navbar-btn" data-toggle="modal"
+                                    data-target="#subscribeModal">
+                                    {{ __('New Feed') }}
+                                </button>
+                            </li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown"
                                     role="button" aria-expanded="false" aria-haspopup="true">
