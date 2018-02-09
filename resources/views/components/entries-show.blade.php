89c9c4a402aefab001988d0acb95a46a7905c9e4
tyabu12 <tomotomo.y.1212@gmail.com>
2018-02-09 18:52:21 +0900

    feat: change view design.

--- resources/views/components/entries-show.blade.php
+++ resources/views/components/entries-show.blade.php
@@ -1,19 +1,23 @@
 @php
-    use Carbon\Carbon;
-    Carbon::setLocale(config('app.locale'));
-    $now = new Carbon();
+    $now = now();
 @endphp
-<div class="table-responsive">
-    <table class="table table-striped table-hover table-condensed">
-        <tbody>
-        @foreach ($entries as $entry)
-            <tr onclick="window.location='{{ url($entry->url) }}';">
-                <td><a href="{{ url('feeds/'.$entry->feed->id) }}">{{ $entry->feed->name }}</a></td>
-                <td><a href="{{ url($entry->url) }}">{{ $entry->title }}</a></td>
-                <td><div class="text-right">{{ $entry->published_at->diffForHumans($now, true) }}</div></td>
-            </a></tr>
-        @endforeach
-        </tbody>
-    </table>
-    {{ $entries->links() }}
+<div class="row row-eq-height">
+    @foreach ($entries as $entry)
+        <div class="col-sm-3 col-xs-6">
+<div class="entry">
+            <div class="title">
+                <h4 title="{{ $entry->title }}">
+                    <a href="{{ url($entry->url) }}">{{ str_limit($entry->title, 80) }}</a>
+                </h4>
+            </div>
+            <div class="feed-name" title="{{ $entry->feed->name }}">
+                <a href="{{ url('feeds/'.$entry->feed->id) }}">{{ str_limit($entry->feed->name, 30) }}</a>
+            </div>
+            <div class="published-at" title="{{ $entry->published_at }}">
+                {{ $entry->published_at->diffForHumans() }}
+            </div>
 </div>
+        </div>
+    @endforeach
+</div>
+<div class="text-right">{{ $entries->links() }}</div>
